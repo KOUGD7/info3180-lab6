ce8f59678b2c8d4b2ffe27c9c371b30393ed347d
KOUGD7 <cougahdere@yahoo.com>
2021-04-01 21:22:01 -0500

    Load Vue App and API

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -51,4 +51,51 @@ app.component('app-footer', {
   }
 })
 
+app.component('news-list', {
+  name: 'NewsList',
+  template: `
+    <div class="news">
+      <h2>News</h2>
+
+      <ul class="news__list d-flex flex-row flex-wrap">
+
+      <div v-for="article in articles" class = "card" style="width: 17rem; margin: 5px;">
+        <div class="card-body">
+            <h5 class="ml-2 mb-0 pb-0 card-title"> {{ article.title }} </h5>
+            <img :src = article.urlToImage class="card-img-top" alt="news image"/>
+
+            <div class="mb-3 mt-1 d-flex flex-row flex-wrap">
+                <p class="card-text"> {{ article.description }} </p>
+            </div>
+
+        </div>
+        <div class = "btn bg-info card-footer text-center"></div>
+      </div>
+      </ul>
+    </div>
+  `,
+
+  created() {
+    let self = this;
+    fetch('https://newsapi.org/v2/top-headlines?country=us',
+  {
+    headers: {
+      'Authorization': 'Bearer <your token>'
+    }
+  })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      self.articles = data.articles;
+    });
+  },
+  data() {
+    return {
+      articles: []
+    }
+  }
+})
+
 app.mount('#app');
